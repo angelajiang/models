c34217607cbd0212bafa2276b0a9f0c4d88b2e0d
Angela Jiang <to.ajiang@gmail.com>
2018-09-06 20:40:43 -0400

    Update bus inception scripts

--- research/inception/scripts/train_bus_inception.sh
+++ research/inception/scripts/train_bus_inception.sh
@@ -1,14 +1,20 @@
 EXP_NAME=$1
 THRESHOLD=$2
 THRESHOLD_DECAY=$3
-LR=$4
+THRESHOLD_DECAY_STEPS=$4
+LR=$5
+LR_DECAY=$6
+LR_DECAY_STEPS=$7
+BATCH_SIZE=$8
 
 DATA_DIR=/proj/BigLearning/ahjiang/datasets/iii-buses-noloss/shards
 INCEPTION_MODEL_DIR=/proj/BigLearning/ahjiang/models
 MODEL_PATH="${INCEPTION_MODEL_DIR}/inception-v3/model.ckpt-157585"
 
-mkdir /proj/BigLearning/ahjiang/output/tmp/$EXP_NAME/
-TRAIN_DIR=/proj/BigLearning/ahjiang/output/tmp/$EXP_NAME/bus_inception_$THRESHOLD"_tdecay"$THRESHOLD_DECAY"_lr"$LR
+TBOARD_DIR="/proj/BigLearning/ahjiang/output/tmp/"$EXP_NAME
+
+mkdir $TBOARD_DIR
+TRAIN_DIR=$TBOARD_DIR"/bus_inception_t"$THRESHOLD"_tdecay"$THRESHOLD_DECAY"_tsteps"$THRESHOLD_DECAY_STEPS"_lr"$LR"_lrdecay"$LR_DECAY"_lrsteps"$LR_DECAY_STEPS"_b"$BATCH_SIZE
 mkdir $TRAIN_DIR
 rm -rf $TRAIN_DIR/*
 
@@ -21,11 +27,12 @@ bazel-bin/inception/bus_train \
   --pretrained_model_checkpoint_path="${MODEL_PATH}" \
   --fine_tune=True \
   --log_gradients=False \
-  --initial_learning_rate=0.001 \
   --input_queue_memory_factor=1 \
-  --batch_size=1 \
+  --batch_size=$BATCH_SIZE \
+  --initial_learning_rate=$LR \
+  --learning_rate_decay_factor=$LR_DECAY \
+  --learning_rate_decay_steps=$LR_DECAY_STEPS \
   --backprop_threshold=$THRESHOLD \
   --threshold_decay_factor=$THRESHOLD_DECAY \
-  --initial_learning_rate=$LR \
-  --learning_rate_decay_steps=$LR_DECAY_STEPS \
+  --threshold_decay_steps=$THRESHOLD_DECAY_STEPS\
 
